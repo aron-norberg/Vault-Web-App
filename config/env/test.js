44749149f19f80db74ad57bd80fb4b553de5e70e
James <jamessandoval>
2018-05-11 14:45:37 -0700

    update test selection for test phase delta. - actual test for images on all pages.

--- config/env/test.js
+++ config/env/test.js
@@ -7,16 +7,16 @@ module.exports = {
 	facebook: {
 		clientID: 'Facebook Application ID',
 		clientSecret: 'Facebook Application Secret',
-		callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+		callbackURL: '/oauth/facebook/callback'
 	},
 	twitter: {
 		clientID: 'Twitter Application ID',
 		clientSecret: 'Twitter Application Secret',
-		callbackURL: 'http://localhost:3000/oauth/twitter/callback'
+		callbackURL: '/oauth/twitter/callback'
 	},
 	google: {
 		clientID: 'Google Application ID',
 		clientSecret: 'Google Application Secret',
-		callbackURL: 'http://localhost:3000/oauth/google/callback'
+		callbackURL: '/oauth/google/callback'
 	}
 };
