132ebf152ff27fe458d6e943eeb4518551369d87
Aron Norberg <aron.norberg@fluke.com>
2018-05-14 12:42:53 -0700

    updated reset_password.ejs

--- app/routes/api_emailer.js
+++ app/routes/api_emailer.js
@@ -1,45 +1,86 @@
 // Invoke 'strict' JavaScript mode
 'use strict';
 
-var nodemailer = require('nodemailer');
-
+const bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
 
 exports.pwd_emailer = function(req, res) {
 
-  // let id = req.query.id;
   // let email = req.query.email;
-  // let pwd = req.query.password;
+  let email = req.body.email;
 
-  let id = req.params.Id;
-  let email = req.params.email;
-  let pwd = req.params.password;
+  //console.log(email);
 
-  console.log(email);
+  //var transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
+
 
   // create reusable transporter object using the default SMTP transport
-  var transporter = nodemailer.createTransport({
-    service: 'smtp-mail.outlook.com',
+  var transporter = nodemailer.createTransport( {
+    host: 'smtp.gmail.com',
     auth: {
-      user: 'youremail@gmail.com',
-      pass: 'yourpassword'
-    }
+      user: 'vault.qa.automation@gmail.com',
+      pass: 'vault@Fluke2018'
+    },
+    tls: { 
+      rejectUnauthorized: false 
+    },
+    proxy: 'socks5://localhost:1080/'
+
   });
+
+
+  // create reusable transporter object using the default SMTP transport
+  /*var transporter = nodemailer.createTransport( {
+    host: 'smtp.gmail.com',
+    port:465,
+    path: '/',
+    method: 'POST',
+    secure: true,
+    headers: {
+      'Content-Type': 'text/html',
+      'Content-Length': Buffer.byteLength("")
+    }, 
+    auth: {
+      user: 'vault.qa.automation@gmail.com',
+      pass: 'vault@Fluke2018'
+    },
+    tls: { 
+      rejectUnauthorized: false 
+    }
+
+  });*/
+  
 
   // setup e-mail data with unicode symbols
   var mailOptions = {
-    from: 'youremail@gmail.com',
+    from: '"Vault password reset" <vault.qa.automation@gmail.com>',
     to: email,
     subject: 'Sending Email using Node.js',
     text: 'That was easy!'
   };
 
+
+  // verify connection configuration
+  /*transporter.verify(function(error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Server is ready to take our messages');
+    }
+  });*/
+
+  
   // send mail with defined transport object
   transporter.sendMail(mailOptions, function(error, info){
+    console.log(mailOptions);
+    //console.log("SENDING");
+
     if (error) {
       console.log(error);
     } else {
       console.log('Email sent: ' + info.response);
     }
+    
   });
 
-}
+} // end exports.pwd_emailer = function(req, res)
