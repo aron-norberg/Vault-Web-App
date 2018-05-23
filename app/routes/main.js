a553f3bdf298c2f421c0be596b0320479f06b8a3
Aron Norberg <aron.norberg@fluke.com>
2018-05-23 10:26:21 -0700

    updated header.ejs, main.js, api_login.js

--- app/routes/main.js
+++ app/routes/main.js
@@ -4,7 +4,8 @@ exports.getHome = function(req, res) {
 
   res.render('home', {
     title: 'Vault Automated Testing Suite',
-    currentUrl: req.url
-    //user: req.user
+    currentUrl: req.url,
+    user: req.user
   });
+  
 };
